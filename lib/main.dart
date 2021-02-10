f6b41042c2e2218c0711d3a8de3fc2392d324f41
Eduardo Couto <eduardocoouto@gmail.com>
2021-02-09 23:51:03 -0300

    feat(firebase): adiciona dependencias

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttergram/app/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-void main() => runApp(ModularApp(module: AppModule()));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('Algo deu errado.'),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ModularApp(module: AppModule());
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
