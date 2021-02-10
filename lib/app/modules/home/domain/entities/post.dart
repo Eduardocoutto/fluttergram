94773d60acc4894bc6f5ab285eb4a0774c0e4a53
Eduardo Couto <eduardocoouto@gmail.com>
2021-02-10 19:00:00 -0300

    feat: adiciona método fromDocument()

--- lib/app/modules/home/domain/entities/post.dart
+++ lib/app/modules/home/domain/entities/post.dart
@@ -1,11 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class PostModel {
   String title;
   String description;
   String photoUrl;
+
+  DocumentReference reference;
 
   PostModel({
     this.title,
     this.description,
     this.photoUrl,
   });
+
+  factory PostModel.fromDocument(DocumentSnapshot doc) {
+    return PostModel(
+        title: doc['title'],
+        description: doc['description'],
+        photoUrl: doc['photoUrl']);
+  }
 }
