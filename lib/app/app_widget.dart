ee4bc03d2fb2c7a2e9e1661676daedf0e784389e
Eduardo Couto <eduardocoouto@gmail.com>
2021-02-11 02:43:00 -0300

    feat: adiciona OKToast

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:asuka/asuka.dart' as asuka show builder;
+import 'package:oktoast/oktoast.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: Modular.navigatorKey,
-      title: 'NoPaperApp',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
+    return OKToast(
+      position: ToastPosition.bottom,
+      handleTouth: true,
+      dismissOtherOnShow: false,
+      child: MaterialApp(
+        navigatorKey: Modular.navigatorKey,
+        title: 'Fluttergram',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: AppBarTheme(color: Colors.black),
+          buttonColor: Colors.black,
+        ),
+        initialRoute: '/',
+        builder: asuka.builder,
+        onGenerateRoute: Modular.generateRoute,
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale('pt', 'BR'),
+        ],
       ),
-      initialRoute: '/',
-      builder: asuka.builder,
-      onGenerateRoute: Modular.generateRoute,
     );
   }
 }
